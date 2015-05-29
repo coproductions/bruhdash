68932e8fe7154da58439678c8ef5399fb08196f5
Constanti <constantin.dumba@gmail.com>
2015-05-29 13:56:57 -1000

    stuck it into a modulus

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -1,248 +1,251 @@
 var global = window || GLOBAL;
 
-global.bruhdash = {
-  chunk: function(array,size){
-    var newArray = [];
-    size = size || 1;
-    for(var i=0; i<array.length; i+=size){
-      newArray.push(array.slice(i,i+size));
-    }
-    return newArray;
-  },
+var bruhdash = (function(){
 
-  compact: function(array) {
-    var newArray = [];
-    for(var i=0; i<array.length; i++){
-      if(array[i]){
-        newArray.push(array[i]);
+  return {
+    chunk: function(array,size){
+      var newArray = [];
+      size = size || 1;
+      for(var i=0; i<array.length; i+=size){
+        newArray.push(array.slice(i,i+size));
       }
-    }
-    return newArray;
-  },
+      return newArray;
+    },
 
-  difference: function(arr1,arr2) {
-    var newArray = [];
-    for(var i=0; i<arr1.length; i++){
-      if(arr2.indexOf(arr1[i])<0){
-        newArray.push(arr1[i]);
-      }
-    }
-    return newArray;
-  },
-
-  drop: function(array,amount){
-    var newArray = array;
-    amount = amount || 1;
-    newArray.splice(0,amount);
-    return newArray;
-  },
-
-  dropRight: function(array,n) {
-    n = n || 1;
-    newArray = array;
-    for(var i=1;i<=n;i++ ){
-      newArray.pop();
-    }
-    return newArray;
-  },
-
-  fill: function(array, value, start, end) {
-    start = start || 0;
-    end = end || array.length;
-    for(var i=start; i<end; i++){
-      array[i]=value;
-    }
-    return array;
-  },
-
-  first: function (array) {
-    return array[0];
-  },
-
-  indexOf: function (array,value,fromIndex) {
-    fromIndex = fromIndex || 0;
-    for(var i=fromIndex; i<array.length; i++){
-      if(array[i]===value){
-        return i;
-      }
-    }
-    return -1;
-  },
-
-  initial: function (array) {
-    return array.slice(0,array.length-1);
-  },
-
-  last: function (array) {
-    return array[array.length-1];
-  },
-
-  lastIndexof: function (array,value,fromIndex) {
-      fromIndex = fromIndex || array.length-1;
-      for(var i=fromIndex; i>=0; i--){
-      if(array[i]===value){
-        return i;
-      }
-    }
-    return -1;
-  },
-
-  pull: function (array,values) {
-    var newArray = [];
-    var counter = 0;
-    for(var i=0; i<array.length; i++){
-      counter = 0;
-      for(var j=1; j<arguments.length; j++){
-        if(array[i]===arguments[j]){
-          counter++;
-        }
-      }
-      if(counter === 0){
-        newArray.push(array[i]);
-      }
-    }
-    return newArray;
-  },
-
-  pullAt: function (array,indexes) {
-    var newArray = [];
-    for(var i=1; i<arguments.length; i++){
-      newArray.push(array[arguments[i]]);
-    }
-    for(var j = arguments.length - 1 ; i > 0 ; j--){
-      array.splice(arguments[j],1);
-    }
-    return newArray;
-  },
-
-  rest: function (array) {
-    array.shift();
-    return array;
-  },
-
-  slice: function (array,start,end) {
-    end = end || array.length;
-    start = start || 0;
-    var newArray = [];
-    for(var i=start; i<end; i++){
-      newArray.push(array[i]);
-    }
-    return newArray;
-  },
-
-  take: function (array,n) {
-    n = n || 1;
-    var newArray = [];
-    for(var i=0; i<=n; i++){
-      newArray.push(array[i]);
-    }
-    return newArray;
-  },
-
-  takeRight: function (array,n) {
-    n = n || 1;
-    var newArray = [];
-    for(var i=array.length-1; i>=array.length-n; i--){
-      newArray.unshift(array[i]);
-    }
-    return newArray;
-  },
-
-  zip: function (arrays) {
-    var n=0;
-    var newArray = [];
-    var tempArray = [];
-    var longest = 0;
-    for (var k = 0; k < arguments.length; k++) {
-      if(longest<arguments[k].length){
-        longest = arguments[k].length;
-      }
-    }
-    while(n++ <longest){
-      for(var i=0; i<arguments.length; i++){
-        if(arguments[i]!== undefined && arguments[i][n]!==undefined){
-          tempArray.push(arguments[i][n]);
-        }
-        else{
-          tempArray.push(null);
-        }
-      }
-      newArray.push(tempArray);
-       tempArray = [];
-    }
-    return newArray;
-  },
-
-  unzip: function (array) {
-    var n=0;
-    var longest = 0;
-    for (var k = 0; k < array.length; k++) {
-      if(array[k].length>longest){
-        longest = array[k].length;
-      }
-    }
-    var newArray = [];
-    var tempArray = [];
-    while(n++ < longest){
+    compact: function(array) {
+      var newArray = [];
       for(var i=0; i<array.length; i++){
-        if(array[i][n] !== undefined && array[i] !== undefined){
-          tempArray.push(array[i][n]);
-        }
-        else{
-          tempArray.push(null);
-        }
-      }
-      newArray.push(tempArray);
-      tempArray = [];
-    }
-    return newArray;
-  },
-
-  without: function(array,exclude) {
-    var newArray = [];
-    var count = 0;
-    for (var i = 0; i< array.length; i++) {
-      for (var j = 1; j < arguments.length; j++) {
-        if(array[i]===arguments[j]){
-          count++;
-        }
-      }
-      if(count===0){
-        newArray.push(array[i]);
-      }
-      count = 0;
-    }
-    return newArray;
-  },
-
-  flatten: function(array,isDeep){
-    var newArray = [];
-    var iterate = function(nestedArray){
-      for (var i = 0; i < nestedArray.length; i++) {
-        if(!Array.isArray(nestedArray[i])){
-          newArray.push(nestedArray[i]);
-        }
-        else{
-          iterate(nestedArray[i]);
-        }
-      }
-    };
-    if(!isDeep){
-      for(var i = 0; i < array.length; i++) {
-        if(Array.isArray(array[i])){
-          for (var j = 0; j < array[i].length; j++) {
-            newArray.push(array[i][j]);
-          }
-        }
-        else{
+        if(array[i]){
           newArray.push(array[i]);
         }
       }
-    }
+      return newArray;
+    },
 
-    else{
-        iterate(array);
+    difference: function(arr1,arr2) {
+      var newArray = [];
+      for(var i=0; i<arr1.length; i++){
+        if(arr2.indexOf(arr1[i])<0){
+          newArray.push(arr1[i]);
+        }
+      }
+      return newArray;
+    },
+
+    drop: function(array,amount){
+      var newArray = array;
+      amount = amount || 1;
+      newArray.splice(0,amount);
+      return newArray;
+    },
+
+    dropRight: function(array,n) {
+      n = n || 1;
+      newArray = array;
+      for(var i=1;i<=n;i++ ){
+        newArray.pop();
+      }
+      return newArray;
+    },
+
+    fill: function(array, value, start, end) {
+      start = start || 0;
+      end = end || array.length;
+      for(var i=start; i<end; i++){
+        array[i]=value;
+      }
+      return array;
+    },
+
+    first: function (array) {
+      return array[0];
+    },
+
+    indexOf: function (array,value,fromIndex) {
+      fromIndex = fromIndex || 0;
+      for(var i=fromIndex; i<array.length; i++){
+        if(array[i]===value){
+          return i;
+        }
+      }
+      return -1;
+    },
+
+    initial: function (array) {
+      return array.slice(0,array.length-1);
+    },
+
+    last: function (array) {
+      return array[array.length-1];
+    },
+
+    lastIndexof: function (array,value,fromIndex) {
+        fromIndex = fromIndex || array.length-1;
+        for(var i=fromIndex; i>=0; i--){
+        if(array[i]===value){
+          return i;
+        }
+      }
+      return -1;
+    },
+
+    pull: function (array,values) {
+      var newArray = [];
+      var counter = 0;
+      for(var i=0; i<array.length; i++){
+        counter = 0;
+        for(var j=1; j<arguments.length; j++){
+          if(array[i]===arguments[j]){
+            counter++;
+          }
+        }
+        if(counter === 0){
+          newArray.push(array[i]);
+        }
+      }
+      return newArray;
+    },
+
+    pullAt: function (array,indexes) {
+      var newArray = [];
+      for(var i=1; i<arguments.length; i++){
+        newArray.push(array[arguments[i]]);
+      }
+      for(var j = arguments.length - 1 ; i > 0 ; j--){
+        array.splice(arguments[j],1);
+      }
+      return newArray;
+    },
+
+    rest: function (array) {
+      array.shift();
+      return array;
+    },
+
+    slice: function (array,start,end) {
+      end = end || array.length;
+      start = start || 0;
+      var newArray = [];
+      for(var i=start; i<end; i++){
+        newArray.push(array[i]);
+      }
+      return newArray;
+    },
+
+    take: function (array,n) {
+      n = n || 1;
+      var newArray = [];
+      for(var i=0; i<=n; i++){
+        newArray.push(array[i]);
+      }
+      return newArray;
+    },
+
+    takeRight: function (array,n) {
+      n = n || 1;
+      var newArray = [];
+      for(var i=array.length-1; i>=array.length-n; i--){
+        newArray.unshift(array[i]);
+      }
+      return newArray;
+    },
+
+    zip: function (arrays) {
+      var n=0;
+      var newArray = [];
+      var tempArray = [];
+      var longest = 0;
+      for (var k = 0; k < arguments.length; k++) {
+        if(longest<arguments[k].length){
+          longest = arguments[k].length;
+        }
+      }
+      while(n++ <longest){
+        for(var i=0; i<arguments.length; i++){
+          if(arguments[i]!== undefined && arguments[i][n]!==undefined){
+            tempArray.push(arguments[i][n]);
+          }
+          else{
+            tempArray.push(null);
+          }
+        }
+        newArray.push(tempArray);
+         tempArray = [];
+      }
+      return newArray;
+    },
+
+    unzip: function (array) {
+      var n=0;
+      var longest = 0;
+      for (var k = 0; k < array.length; k++) {
+        if(array[k].length>longest){
+          longest = array[k].length;
+        }
+      }
+      var newArray = [];
+      var tempArray = [];
+      while(n++ < longest){
+        for(var i=0; i<array.length; i++){
+          if(array[i][n] !== undefined && array[i] !== undefined){
+            tempArray.push(array[i][n]);
+          }
+          else{
+            tempArray.push(null);
+          }
+        }
+        newArray.push(tempArray);
+        tempArray = [];
+      }
+      return newArray;
+    },
+
+    without: function(array,exclude) {
+      var newArray = [];
+      var count = 0;
+      for (var i = 0; i< array.length; i++) {
+        for (var j = 1; j < arguments.length; j++) {
+          if(array[i]===arguments[j]){
+            count++;
+          }
+        }
+        if(count===0){
+          newArray.push(array[i]);
+        }
+        count = 0;
+      }
+      return newArray;
+    },
+
+    flatten: function(array,isDeep){
+      var newArray = [];
+      var iterate = function(nestedArray){
+        for (var i = 0; i < nestedArray.length; i++) {
+          if(!Array.isArray(nestedArray[i])){
+            newArray.push(nestedArray[i]);
+          }
+          else{
+            iterate(nestedArray[i]);
+          }
+        }
+      };
+      if(!isDeep){
+        for(var i = 0; i < array.length; i++) {
+          if(Array.isArray(array[i])){
+            for (var j = 0; j < array[i].length; j++) {
+              newArray.push(array[i][j]);
+            }
+          }
+          else{
+            newArray.push(array[i]);
+          }
+        }
+      }
+
+      else{
+          iterate(array);
+      }
+      return newArray;
     }
-    return newArray;
-  }
-};
+  };
+})();
